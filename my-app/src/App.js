21dd26ba9fdbf7e61562d1c323adb9e60bd212b8
Steven Peng <danceofthepkz111@gmail.com>
2024-08-01 03:36:41 +0800

    made changes to the router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,8 @@
 
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import shelterCost from './components/categories/shelterCost'
-// import medicalCost from './components/categories/medicalCost'
-// import childrensCost from './components/categories/childrensCost'
-// import encampmentCost from './components/categories/encampmentCost'
-// import FinalReview from './components/FinalReview'; //for reviewing all inputs before final submission
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
-import logo from './logo.svg';
 import './App.css';
 import './styles.css';
 
@@ -18,18 +13,28 @@ import Form from './components/features/Form';
 // import Footer from './components/layout/Footer';
 
 
-
-
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/form" element={<Form />} />
       </Routes>
-    </Router>
-      
+    </HashRouter>
   );
 }
+
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//           <Route path="/" element={<Homepage />} />
+//           <Route path="/form" element={<Form />} />
+//       </Routes>
+//     </Router>
+      
+//   );
+// }
 
 export default App;
