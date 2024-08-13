5d8c6a2bade937fa84a65754ffcd83568f20c9b3
Steven Peng <danceofthepkz111@gmail.com>
2024-08-13 11:48:54 +0800

    on progress most recent version.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,11 +6,13 @@ import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import './styles.css';
 
-// src/App.js
 // import Header from './components/layout/Header';
 import Homepage from './components/Homepage';
 import Form from './components/features/Form';
+import ReviewPage from './components/ReviewPage';
+import CalculatePage from './components/calculatePage';
 // import Footer from './components/layout/Footer';
+
 
 
 function App() {
@@ -19,22 +21,13 @@ function App() {
       <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/form" element={<Form />} />
+          <Route path="/review" element={<ReviewPage />} />
+          <Route path="/calculate" element={<CalculatePage />} />
       </Routes>
     </HashRouter>
   );
 }
 
 
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//           <Route path="/" element={<Homepage />} />
-//           <Route path="/form" element={<Form />} />
-//       </Routes>
-//     </Router>
-      
-//   );
-// }
 
 export default App;
